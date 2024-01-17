5748991c4aca74158abf691876fb4029062f4159
AngleReyes <156244781+AngleReyes@users.noreply.github.com>
2024-01-17 14:47:42 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,79 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "fridge":return img`
+................
+................
+................
+33333...........
+111113..........
+1111133.........
+111113133.......
+1111131113......
+11111311113.....
+111113111113....
+11111311111133..
+111113111111313.
+1111131e11113113
+11111311ee113113
+1111131111e13113
+1111133311113113
+1111131133113e13
+1111131111333e13
+1111131111113e13
+1111131e11113e13
+11111311ee113e13
+1111131111e13e13
+1111131111113113
+1111131111113113
+1111131111113113
+1111131111113113
+3333333111113113
+eeeeeee331113113
+eeeeeeeee3313113
+eeeeeeeeeee33113
+eeeeeeeeeeeee333
+3333333333333333
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.

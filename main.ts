b50bc3e01eca7981eb04343dfbd68dbff3225804
AngleReyes <156244781+AngleReyes@users.noreply.github.com>
2024-01-29 14:16:03 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -162,11 +162,20 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     )
 })
 function doSomething (num: number) {
-    for (let index = 0; index < num; index++) {
-        SpawnLocation = tiles.getTilesByType(assets.tile`myTile7`)
-        Customers = sprites.create(CustomerImages._pickRandom(), SpriteKind.Customer)
-        tiles.placeOnRandomTile(Customers, assets.tile`myTile7`)
-        Customers.setVelocity(-20, 0)
+    if (num < CustomerImages.length) {
+        for (let index = 0; index < num; index++) {
+            SpawnLocation = tiles.getTilesByType(assets.tile`myTile7`)
+            Customers = sprites.create(CustomerImages._pickRandom(), SpriteKind.Customer)
+            tiles.placeOnRandomTile(Customers, assets.tile`myTile7`)
+            Customers.setVelocity(randint(-10, -30), 0)
+        }
+    } else if (num == CustomerImages.length) {
+        for (let index = 0; index < num; index++) {
+            SpawnLocation = tiles.getTilesByType(assets.tile`myTile7`)
+            Customers = sprites.create(CustomerImages._pickRandom(), SpriteKind.Customer)
+            tiles.placeOnRandomTile(Customers, assets.tile`myTile7`)
+            Customers.setVelocity(randint(-40, -50), 0)
+        }
     }
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -253,22 +262,20 @@ let mySprite: Sprite = null
 let CustomerImages: Image[] = []
 CustomerImages = [
 img`
-    . . . . f f f f f f . . . . . . 
-    . . . f 2 f e e e e f f . . . . 
-    . . f 2 2 2 f e e e e f f . . . 
-    . . f e e e e f f e e e f . . . 
-    . f e 2 2 2 2 e e f f f f . . . 
-    . f 2 e f f f f 2 2 2 e f . . . 
-    . f f f e e e f f f f f f f . . 
-    . f e e 4 4 f b e 4 4 e f f . . 
-    . . f e d d f 1 4 d 4 e e f . . 
-    . . . f d d d d 4 e e e f . . . 
-    . . . f e 4 4 4 e e f f . . . . 
-    . . . f 2 2 2 e d d 4 . . . . . 
-    . . . f 2 2 2 e d d e . . . . . 
-    . . . f 5 5 4 f e e f . . . . . 
-    . . . . f f f f f f . . . . . . 
-    . . . . . . f f f . . . . . . . 
+    . . . . . . . . . . . . . . 
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e b f b . 
+    f d d f d d f d d f f d f . 
+    f b d d b b d d 2 b f d f . 
+    . f 2 2 2 2 2 2 d b d b f . 
+    . f d d d d d d d f f f . . 
+    . f d b d f f f d f . . . . 
+    . . f f f f . . f f . . . . 
     `,
 img`
     ........................
@@ -315,22 +322,22 @@ img`
     . . . . . . f f f . . . . . . . 
     `,
 img`
-    . . . . f f f f f . . . . . . . 
-    . . . f e e e e e f . . . . . . 
-    . . f d d d d e e e f . . . . . 
-    . c d f d d f d e e f f . . . . 
-    . c d f d d f d e e d d f . . . 
-    c d e e d d d d e e b d c . . . 
-    c d d d d c d d e e b d c . . . 
-    c c c c c d d e e e f c . . . . 
-    . f d d d d e e e f f . . . . . 
-    . . f f f f f e e e e f . . . . 
-    . . . . f f e e e e e e f . f f 
-    . . . f e e f e e f e e f . e f 
-    . . f e e f e e f e e e f . e f 
-    . f b d f d b f b b f e f f e f 
-    . f d d f d d f d d b e f f f f 
-    . . f f f f f f f f f f f f f . 
+    . f f f . . . . . . . . f f f . 
+    f f c . . . . . . . f c b b c . 
+    f c c . . . . . . f c b b c . . 
+    c f . . . . . . . f b c c c . . 
+    c f f . . . . . f f b b c c . . 
+    f f f c c . c c f b c b b c . . 
+    f f f c c c c c f b c c b c . . 
+    . f c 3 c c 3 b c b c c c . . . 
+    . c b 3 b c 3 b b c c c c . . . 
+    c c b b b b b b b b c c . . . . 
+    c b 1 b b b 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    f b c b b b c b b b b f . . . . 
+    . f 1 f f f 1 b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
     `
 ]
 tiles.setCurrentTilemap(tilemap`Cafe`)
@@ -489,6 +496,9 @@ game.onUpdate(function () {
     }
 })
 game.onUpdate(function () {
+	
+})
+game.onUpdate(function () {
     Distance = Math.sqrt((mySprite.x - Fridge.x) * (mySprite.x - Fridge.x) + (mySprite.y - Fridge.y) * (mySprite.y - Fridge.y))
     if (Distance <= Access) {
         Fridge.setImage(assets.image`fridgeh`)
@@ -506,4 +516,7 @@ game.onUpdate(function () {
 })
 game.onUpdateInterval(15000, function () {
     doSomething(randint(0, CustomerImages.length))
+})
+game.onUpdateInterval(16000, function () {
+    Customers.setVelocity(50, 0)
 })
